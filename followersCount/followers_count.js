a99b587ba7c84b96b3eba2d78d9d201831c44810
Durodola Abdulhameed <durodolaabdulhameed2021@gmail.com>
2025-10-16 10:43:54 -0400

    followers Count

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,12 @@ function increaseCount() {
   checkCountValue(); // Check count value and display messages
 }
 
+function resetCount(){
+    count = 0;
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
@@ -15,5 +21,7 @@ function checkCountValue() {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
+  } else if (count === 0){
+    alert("Followers count has been reset.");
   }
 }
